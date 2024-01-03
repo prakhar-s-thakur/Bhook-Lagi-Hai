cbd0231f80f04a500bd4e0bc16020f3668fbd27a
prakhar-s-thakur <prakharthakur869@gmail.com>
2024-01-02 17:09:47 -0700

    created components

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://th.bing.com/th/id/OIG.xDawWvI8xBBxeBpQ2QS2?w=1024&h=1024&rs=1&pid=ImgDetMain";
